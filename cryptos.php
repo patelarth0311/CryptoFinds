17a77cd5ca4aa9685c940935e98e396fcfa43c53
sagarasthana <91163467+sagarasthana@users.noreply.github.com>
2022-04-19 16:34:34 -0400

    Add files via upload

--- cryptos.php
+++ cryptos.php
@@ -1,12 +1,28 @@
-<?php  include_once 'header.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link  rel = "stylesheet" href = "styles/styles3.css" > </link>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Cryptos Page</title>
+</head>
+<body>
 
 <div id = "container" >
+  
+<form action="search.php" method="post">
+  <div id="search" >
+  <input type = "text" id="keyword" name="keyword" placeholder = "Search" ></input>
+</form>
 
-<div id="search" >
-
-<input type = "text" placeholder = "Search" ></input>
+<div id="addwishlist">
+        <a href="checkingpersonalwishlist.php">View your Wishlist</a>
+    </div>
 
 </div>
+
 <?php
 session_start();
 include('includes/dbh.inc.php');
---
+++ search.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<link  rel = "stylesheet" href = "styles/styles3.css" > </link>
+</html>
+
+<?php
+if (isset($_POST["keyword"])) {
+
+$url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+$parameters = [
+  'limit' => "50"
+];
+
+$headers = [
+  'Accepts: application/json',
+  'X-CMC_PRO_API_KEY: 0a6c72f1-d71b-4932-81d8-1b865951aac7'
+];
+$qs = http_build_query($parameters); // query string encode the parameters
+$request = "{$url}?{$qs}"; // create the request URL
+
+
+$curl = curl_init(); // Get cURL resource
+// Set cURL options
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $request,            // set the request URL
+  CURLOPT_HTTPHEADER => $headers,     // set the headers 
+  CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+));
+
+$response = curl_exec($curl); // Send the request, save the response
+ // print json decoded response
+
+  
+$decoded_json = json_decode($response, true); 
+$name = $decoded_json["data"];
+
+// price 24h 7d market cap volume supply 
+
+for ($i = 0; $i < count($name); $i++) {
+    $toLower = strtolower($name[$i]['name']);
+    $toLower2 = strtolower($name[$i]['symbol']);
+    $toLower  = str_replace(' ', '-', $toLower);
+    $symbol = $name[$i]['symbol'];
+    $price = number_format($name[$i]['quote']['USD']['price'], 2, '.', '');
+    $volume24 = $name[$i]['quote']['USD']['volume_change_24h'];
+    $volume7day = $name[$i]['quote']['USD']['percent_change_7d'];
+    $marketcap = $name[$i]['quote']['USD']['market_cap'];
+    $volume = $name[$i]['quote']['USD']['volume_24h'];
+    $supply = $name[$i]['circulating_supply'];
+
+    if($name[$i]['name']==$_POST["keyword"]) {
+        echo  "<div class = 'row'>
+         <img src = 'images/star.svg'></img>
+         <img class = 'cryptopics' src = 'https://cryptologos.cc/logos/{$toLower}-{$toLower2}-logo.svg?v=022' ></img>
+         <p>{$name[$i]['name']}</p>
+         <p>{$symbol}</p>
+         <p>{$price }</p>
+         <p>{$volume24}</p>
+         <p>{$volume7day}</p>
+         <p>{$marketcap}</p>
+         <p>{$volume}</p>
+         <p>{$supply}</p>
+         <button style='background-color: green;' onclick='location.href=\"cryptos.php?add_id=".$name[$i]['name']."\";'>Add</button>
+         </div> ";
+
+    } 
+    
+    }
+    if(isset($_REQUEST['add_id'])) {
+      $add_id=$_REQUEST['add_id'];
+      $usersId=$_SESSION['userid'];
+      $sql="SELECT* FROM wishlist WHERE usersId='$usersId' AND cryptoName='$add_id'";
+      $result=mysqli_query($conn, $sql);
+      if(mysqli_num_rows($result)==1) {
+        echo '<script>alert("Already in wishlist")</script>';
+      }
+      
+      else {
+      $insert="INSERT INTO wishlist (cryptoName, usersId) VALUES ('$add_id', '$usersId')";
+      if(mysqli_query($conn, $insert)) {
+        echo '<script>alert("Successfully added to wishlist")</script>';
+      }
+  }
+  }
+}
+
+curl_close($curl); // Close request
+
+?>
